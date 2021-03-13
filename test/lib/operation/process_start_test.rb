ba354c1ee4e8b30e88753056ac14e36f30ab00bc
James Prior <james@mabonus.net>
2021-03-13 12:49:42 -0600

    Use generic uname instead of specifying path

--- test/lib/operation/process_start_test.rb
+++ test/lib/operation/process_start_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class Operation::ProcessStartTest < Minitest::Test
   def test_event
-    event = Operation::ProcessStart.new(path: '/usr/bin/uname', arguments: '-a').invoke
+    event = Operation::ProcessStart.new(path: 'uname', arguments: '-a').invoke
     assert_equal 'ProcessStart', event.activity_type
 
     assert event.timestamp
     assert_equal Process.euid, event.username
-    assert_equal '/usr/bin/uname -a', event.process_cmd
+    assert_equal 'uname -a', event.process_cmd
     assert event.process_id
   end
 end
